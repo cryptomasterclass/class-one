f00eaf0085ae4cd9b42e6a2f4dea621311c9eda2
rumi1359 <ali.yazdan@aol.com>
2018-02-06 22:19:08 -0800

    v10-deploy-to-rinkeby

---
+++ scripts/deploytorinkeby.sh
@@ -0,0 +1,1 @@
+rm -rf ./build | truffle deploy --network rinkeby --reset
--- truffle.js
+++ truffle.js
@@ -8,6 +8,12 @@ module.exports = {
       network_id: "*", // Match any network id
       gas: 6385876,
       gasPrice: 20000000000
+    },
+    rinkeby: {
+      host: "127.0.0.1", //localhost
+      port: 8545,
+      network_id: 4,
+      gasPrice: 20000000000
     }
   }
 };
